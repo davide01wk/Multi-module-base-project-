3fa17670a9235482370abe551a4d0a3112fe6d6a
Davide Meloni <d.meloni@contrader.it>
2024-09-16 10:42:09 +0200

    Improve navigation

--- onboarding/presentation/src/main/java/com/deme/presentation/weight/WeightViewModel.kt
+++ onboarding/presentation/src/main/java/com/deme/presentation/weight/WeightViewModel.kt
@@ -55,7 +55,7 @@ class WeightViewModel @Inject constructor(
                 return@launch
             }
             preferences.saveWeight(weightNumber)
-            _uiEvent.send(UiEvent.Navigate(Route.ACTIVITY)) //todo
+            _uiEvent.send(UiEvent.Navigate(Route.ACTIVITY))
         }
     }
 }
