c9de741d6c4bce6a717666df2c4472ce7e5865b1
Davide Meloni <d.meloni@contrader.it>
2024-09-30 11:35:06 +0200

    Fix TrackFood Usecase

--- tracker/domain/src/main/java/com/deme/domain/usecase/TrackFood.kt
+++ tracker/domain/src/main/java/com/deme/domain/usecase/TrackFood.kt
@@ -5,6 +5,7 @@ import com.deme.domain.model.TrackableFood
 import com.deme.domain.model.TrackedFood
 import com.deme.domain.repository.TrackerRepo
 import java.time.LocalDate
+import kotlin.math.roundToInt
 
 class TrackFood(
     private val repository: TrackerRepo
@@ -17,14 +18,14 @@ class TrackFood(
     ) {
         val trackedFood = TrackedFood(
             name = food.name,
-            carbs = food.carbsPer100g * (amount / 100),
-            protein = food.proteinPer100g * (amount / 100),
-            fat = food.fatPer100g * (amount / 100),
+            carbs = (food.carbsPer100g * (amount / 100.0)).roundToInt(),
+            protein = (food.proteinPer100g * (amount / 100.0)).roundToInt(),
+            fat = (food.fatPer100g * (amount / 100.0)).roundToInt(),
             imageUrl = food.imageUrl,
             mealType = mealType,
             amount = amount,
             date = date,
-            calories = food.caloriesPer100g * (amount / 100)
+            calories = (food.caloriesPer100g * (amount / 100.0)).roundToInt()
         )
         repository.insertTrackedFood(
             food = trackedFood
