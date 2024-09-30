fe3b15286d3047ff43b6e7cd16cd060e1b5d5adc
Davide Meloni <d.meloni@contrader.it>
2024-09-30 11:39:24 +0200

    Add Navigation SearchRoute & onboarding check

--- app/src/main/java/com/deme/calorytracker/MainActivity.kt
+++ app/src/main/java/com/deme/calorytracker/MainActivity.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.deme.calorytracker.navigation.navigate
+import com.deme.domain.preferences.Preferences
 import com.deme.presentation.activity.ActivityLevelRoute
 import com.deme.presentation.age.AgeRoute
 import com.deme.presentation.gender.GenderRoute
@@ -19,14 +22,20 @@ import com.deme.presentation.goal.GoalRoute
 import com.deme.presentation.height.HeightRoute
 import com.deme.presentation.navigation.Route
 import com.deme.presentation.nutrient_goal.NutrientGoalRoute
+import com.deme.presentation.search.SearchRoute
 import com.deme.presentation.theme.CaloryTrackerTheme
 import com.deme.presentation.tracker_overview.TrackerOverviewRoute
 import com.deme.presentation.weight.WeightRoute
 import com.deme.presentation.welcome.WelcomeRoute
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,61 +49,90 @@ class MainActivity : ComponentActivity() {
                     NavHost(
                         modifier = Modifier.padding(contentPadding),
                         navController = navController,
-                        startDestination = Route.WELCOME
+                        startDestination =
+                        if (preferences.loadShowOnboarding()) Route.WELCOME
+                        else Route.TRACKER_OVERVIEW
                     ) {
-                        composable(Route.WELCOME){
+                        composable(Route.WELCOME) {
                             WelcomeRoute(
                                 onNavigateToGender = navController::navigate
                             )
                         }
-                        composable(Route.AGE){
+                        composable(Route.AGE) {
                             AgeRoute(
                                 scaffoldState = scaffoldState,
                                 onNavigateToHeight = navController::navigate
                             )
                         }
-                        composable(Route.GENDER){
+                        composable(Route.GENDER) {
                             GenderRoute(
                                 onNavigateToAge = navController::navigate
                             )
                         }
-                        composable(Route.HEIGHT){
+                        composable(Route.HEIGHT) {
                             HeightRoute(
                                 scaffoldState = scaffoldState,
                                 onNavigateToWeight = navController::navigate
                             )
                         }
-                        composable(Route.WEIGHT){
+                        composable(Route.WEIGHT) {
                             WeightRoute(
                                 scaffoldState = scaffoldState,
                                 onNavigateToActivityLevel = navController::navigate
                             )
                         }
-                        composable(Route.NUTRIENT_GOAL){
+                        composable(Route.NUTRIENT_GOAL) {
                             NutrientGoalRoute(
                                 scaffoldState = scaffoldState,
                                 onNavigateToTracker = navController::navigate
                             )
                         }
-                        composable(Route.ACTIVITY){
+                        composable(Route.ACTIVITY) {
                             ActivityLevelRoute(
                                 onNavigateToGoal = navController::navigate
                             )
                         }
-                        composable(Route.GOAL){
+                        composable(Route.GOAL) {
                             GoalRoute(
                                 onNavigateToNutrientGoal = navController::navigate
                             )
                         }
-                        composable(Route.TRACKER_OVERVIEW){
+                        composable(Route.TRACKER_OVERVIEW) {
                             TrackerOverviewRoute(
-                                onNavigateToSearch = {
-
-                                }
+                                onNavigateToSearch = navController::navigate
                             )
                         }
-                        composable(Route.SEARCH){
-
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") {
+                                    type = NavType.StringType
+                                },
+                                navArgument("dayOfMonth") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("month") {
+                                    type = NavType.IntType
+                                },
+                                navArgument("year") {
+                                    type = NavType.IntType
+                                },
+                            )
+                        ) {
+                            val mealName = it.arguments?.getString("mealName")!!
+                            val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                            val month = it.arguments?.getInt("month")!!
+                            val year = it.arguments?.getInt("year")!!
+                            SearchRoute(
+                                mealName = mealName,
+                                dayOfMonth = dayOfMonth,
+                                month = month,
+                                year = year,
+                                navigateUp = {
+                                    navController.navigateUp()
+                                },
+                                scaffoldState = scaffoldState
+                            )
                         }
                     }
                 }
